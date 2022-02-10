7056cd2c7ba7b900bc29c9b0e53bafabac72b737
ARID412 <98037006+ARID412@users.noreply.github.com>
2022-02-10 08:50:36 +0700

    Add files via upload

---
+++ kendaraan.js
@@ -0,0 +1,18 @@
+class Atuthentication {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
+class Login extends Atuthentication {
+    login() {
+        console.log('berhasil');
+    }
+}
+
+
+let login = new Login('zul', '1');
+login.login() {
+    consele.log(username)
+}
